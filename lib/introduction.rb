6426e2806c414052665b825b31ccfea44d605115
Becky Phan <beckyphan@live.com>
2019-05-12 14:32:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,6 @@ def introduction(name)
   puts "Hi,m my name is #{name}."
 end
 
+def introduction_with_language(name, language)
+    puts "Hi,m my name is #{name} and I am learning toprogram in #{language}."
+end
